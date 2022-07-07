088c38d682d573c5edfe1bd407994b08d86700b6
dennis <wuaoliu52@gmail.com>
2022-07-07 16:33:57 -0400

    add transmission_estimate

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ import cv2
 def get_dark_channel(img, win_size):
     H, W, C = img.shape
     pad_size = math.floor(win_size/2)
-    print(pad_size)
+    # print(pad_size)
     r, g, b = img[..., 0], img[..., 1], img[..., 2]
     padded_r = np.pad(r, pad_size, 'constant', constant_values=np.inf)
     padded_g = np.pad(g, pad_size, 'constant', constant_values=np.inf)
     padded_b = np.pad(b, pad_size, 'constant', constant_values=np.inf)
-    print(padded_r.shape)
-    print(padded_g.shape)
-    print(padded_b.shape)
+    # print(padded_r.shape)
+    # print(padded_g.shape)
+    # print(padded_b.shape)
     padded_img = np.stack((padded_r, padded_g, padded_b), axis=-1)
-    print(padded_img.shape)
+    # print(padded_img.shape)
     dark_channel = np.zeros((H, W))
     for i in range(H):
         for j in range(W):
             patch = padded_img[i:i+win_size, j:j+win_size, :]
-            print(patch.shape)
-            print(np.min(patch))
+            # print(patch.shape)
+            # print(np.min(patch))
             dark_channel[i][j] = np.min(patch)
     return dark_channel
 
@@ -38,9 +38,13 @@ def get_atmosphere(img, dark):
     atmosphere = accumulator / n_search_pixels
     return atmosphere
 
-def get_transmission_estimate(img, A, omega, win_size):
-
-    pass
+def get_transmission_estimate(img, atmosphere, omega, win_size):
+    H, W, C = img.shape
+    A = np.tile(atmosphere.reshape(1, 1, 3), (H, W, 1))
+    print(A)
+    print(A.shape)
+    trans_est = 1.0 - omega * get_dark_channel(img / A, win_size)
+    return trans_est
 
 
 def dehaze(image, omega=0.95, win_size=15, Lambda = 0.0001):
@@ -48,6 +52,7 @@ def dehaze(image, omega=0.95, win_size=15, Lambda = 0.0001):
     dark_channel = get_dark_channel(image, win_size)
     atmosphere = get_atmosphere(image, dark_channel)
     print(atmosphere)
+    trans_est = get_transmission_estimate(image, atmosphere, omega, win_size)
 
     # plt.imshow(dark_channel)
     # plt.show()
