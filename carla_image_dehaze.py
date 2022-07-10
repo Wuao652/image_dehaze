e7b77c55da849f87e6a56683a55121a95f5e64d4
dennis <wuaoliu52@gmail.com>
2022-07-10 18:04:09 -0400

    dehaze the first 10 imgs in carla dataset

--- carla_image_dehaze.py
+++ carla_image_dehaze.py
@@ -19,18 +19,17 @@ def find_files(dir, exts):
         return []
 
 if __name__ == "__main__":
-    # hazy_img_path = "/home/dennis/nerfplusplus/data/carla_data/hazy/9actors"
-    # img_files = find_files(hazy_img_path, exts=['*.png', '*.jpg'])
-    # # img = imageio.imread(img_files[0])[..., ::-1].astype(np.float32) / 255.
-    # img = cv2.imread(img_files[0]).astype(np.float64) / 255.
-    # cv2.imshow("input", img)
-
-    img = np.load("./data/image.npy")[..., ::-1].astype(np.float64)
-    result, dark, coarse_t, fine_t, A = dehaze(img, 0.95, 15, 0.0001)
-    result = np.clip(result, 0., 1.)
-    cv2.imshow("dark", dark)
-    cv2.imshow("coarse_t", coarse_t)
-    cv2.imshow("fine_t", fine_t)
-    cv2.imshow('J', result)
-    cv2.waitKey()
-
+    hazy_img_path = "/home/dennis/nerfplusplus/data/carla_data/hazy/9actors"
+    img_files = find_files(hazy_img_path, exts=['*.png', '*.jpg'])
+    os.makedirs(f"./carla_results", exist_ok=True)
+    for i in range(10):
+        print(f"start {str(i)} image ======>>>>>>")
+        img = cv2.imread(img_files[i]).astype(np.float64) / 255.
+        # img = np.load("./data/image.npy")[..., ::-1].astype(np.float64)
+        result, dark, coarse_t, fine_t, A = dehaze(img, 0.95, 15, 0.0001)
+        result = np.clip(result, 0., 1.)
+        cv2.imwrite(f"./carla_results/dark_r_{str(i).zfill(5)}.png", dark * 255.)
+        cv2.imwrite(f"./carla_results/coarse_t_r_{str(i).zfill(5)}.png", coarse_t * 255.)
+        cv2.imwrite(f"./carla_results/fine_t_r_{str(i).zfill(5)}.png", fine_t * 255.)
+        cv2.imwrite(f"./carla_results/clear_r_{str(i).zfill(5)}.png", result * 255.)
+        break
