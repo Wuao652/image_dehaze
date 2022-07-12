99f751674ae472a4c32ee4c5d53db2ce498e19ff
dennis <wuaoliu52@gmail.com>
2022-07-12 00:23:11 -0400

    generate t and A for nerf++

--- carla_image_dehaze.py
+++ carla_image_dehaze.py
@@ -21,15 +21,18 @@ def find_files(dir, exts):
 if __name__ == "__main__":
     hazy_img_path = "/home/dennis/nerfplusplus/data/carla_data/hazy/9actors"
     img_files = find_files(hazy_img_path, exts=['*.png', '*.jpg'])
-    os.makedirs(f"./carla_results", exist_ok=True)
-    for i in range(10):
+    os.makedirs(f"/home/dennis/nerfplusplus/data/carla_data/hazy/9actors_trans", exist_ok=True)
+    for i in range(len(img_files)):
         print(f"start {str(i)} image ======>>>>>>")
         img = cv2.imread(img_files[i]).astype(np.float64) / 255.
         # img = np.load("./data/image.npy")[..., ::-1].astype(np.float64)
         result, dark, coarse_t, fine_t, A = dehaze(img, 0.95, 15, 0.0001)
         result = np.clip(result, 0., 1.)
-        cv2.imwrite(f"./carla_results/dark_r_{str(i).zfill(5)}.png", dark * 255.)
-        cv2.imwrite(f"./carla_results/coarse_t_r_{str(i).zfill(5)}.png", coarse_t * 255.)
-        cv2.imwrite(f"./carla_results/fine_t_r_{str(i).zfill(5)}.png", fine_t * 255.)
-        cv2.imwrite(f"./carla_results/clear_r_{str(i).zfill(5)}.png", result * 255.)
-        break
+        # cv2.imwrite(f"./carla_results/dark_r_{str(i).zfill(5)}.png", dark * 255.)
+        # cv2.imwrite(f"./carla_results/coarse_t_r_{str(i).zfill(5)}.png", coarse_t * 255.)
+        # cv2.imwrite(f"./carla_results/fine_t_r_{str(i).zfill(5)}.png", fine_t * 255.)
+        # cv2.imwrite(f"./carla_results/clear_r_{str(i).zfill(5)}.png", result * 255.)
+        print(A)
+        print(A[:, ::-1])
+        np.savetxt(f"/home/dennis/nerfplusplus/data/carla_data/hazy/9actors_trans/a_{str(i).zfill(5)}.txt", A[:, ::-1])
+        cv2.imwrite(f"/home/dennis/nerfplusplus/data/carla_data/hazy/9actors_trans/t_{str(i).zfill(5)}.png", coarse_t * 255.)
